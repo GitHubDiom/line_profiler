47d90a810c11959da13bcc18ca8bb60b216a7b79
GitHubDiom <867320164@qq.com>
2022-07-01 16:59:34 +0800

    Add info log

--- run-private-pypi.sh
+++ run-private-pypi.sh
@@ -1,5 +1,11 @@
-# kill if there is a pypi-server running
-kill $(ps -ef | grep pypi-server | grep -v grep | awk '{print $2}')
+# Kill if there is a pypi-server running
+running_pypi_server=$(ps -ef | grep pypi-server | grep -v grep | awk '{print $2}')
 
-echo "pypi-server started"
-pypi-server run -p 8822 -P . -a . ./packages
+if [ "${running_pypi_server}" ]; then
+    echo  "There is a running pypi-server, now kill it."
+    kill ${running_pypi_server}
+fi
+
+
+echo "Starting pypi-server..."
+nohup pypi-server run -p 8822 -P . -a . ./packages --overwrite >pypi-server.log &
