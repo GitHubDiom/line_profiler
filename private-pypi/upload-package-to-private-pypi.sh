a7a32cb832d05193548445dd98f1ee152c45f843
GitHubDiom <867320164@qq.com>
2022-07-05 22:32:50 +0800

    Limite py version to 37

--- private-pypi/upload-package-to-private-pypi.sh
+++ private-pypi/upload-package-to-private-pypi.sh
@@ -1,4 +1,12 @@
 cd ..
+PY_VERSION=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $1$2}'`
+
+if (( $PY_VERSION != 37 ))
+then
+    echo "Please using python 3.7.x for building this package"
+    exit
+fi
+
 python setup.py check
 python setup.py sdist bdist_wheel
 
